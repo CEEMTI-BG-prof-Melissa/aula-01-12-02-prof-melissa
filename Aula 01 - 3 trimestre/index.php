0048516eeaf2a44ea27ba7c9abadd69b776d53cc
melschreder <167876103+melschreder@users.noreply.github.com>
2025-09-03 10:38:02 -0300

    Create index.php

---
+++ Aula 01 - 3 trimestre/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Aula 01 PHP</title>
+    </head>
+    <body>
+        <?php
+            echo "Olá, Mundo!";
+            echo "Melissa";
+        ?>
+    </body> 
+</html>
